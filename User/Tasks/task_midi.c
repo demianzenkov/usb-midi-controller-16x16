60c8b8ad6c06e24be7d10717f91a5887c466338c
Demian Zenkov <zenkov94@mail.ru>
2024-05-09 11:01:48 +0300

    MIDI input handler added

--- User/Tasks/task_midi.c
+++ User/Tasks/task_midi.c
@@ -55,3 +55,18 @@ void TaskMIDI_task(void const *arg) {
         }
 	}
 }
+
+void USBD_MIDI_DataInHandler(uint8_t *usb_rx_buffer, uint8_t usb_rx_buffer_length)
+{
+  while (usb_rx_buffer_length && *usb_rx_buffer != 0x00)
+  {
+//    cable = usb_rx_buffer[0] >> 4;
+//    code = usb_rx_buffer[0] & 0x0F;
+//    message = usb_rx_buffer[1] >> 4;
+//    channel = usb_rx_buffer[1] & 0x0F;
+//    messageByte1 = usb_rx_buffer[2];
+//    messageByte2 = usb_rx_buffer[3];
+    usb_rx_buffer += 4;
+    usb_rx_buffer_length -= 4;
+  }
+}
